9202ea7e7801763b173aca1d8dc3abe21a416307
vladius9512 <dabija_vlad_adrian@yahoo.com>
2023-11-14 12:37:53 +0200

    Added a frequentquestions component and added some more properties to the flashcard object

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import FlashCard from "./components/FlashCard";
 import Navigation from "./components/Navigation";
+import FrequentQuestions from "./components/FrequentQuestions";
 
 function App() {
     const [cardInfo, setCardInfo] = useState([
         {
+            name: "frecventCancer",
+            keywords: ["cancer", "femei", "frecvent"],
             frontInfo: "Care este cel mai frecvent cancer la femei?",
             backInfo:
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
@@ -14,6 +17,7 @@ function App() {
         <>
             <Navigation></Navigation>
             <FlashCard cardInformations={cardInfo[0]}></FlashCard>
+            <FrequentQuestions></FrequentQuestions>
         </>
     );
 }
---
+++ src/components/FrequentQuestions.jsx
@@ -0,0 +1,41 @@
+function FrequentQuestions() {
+    return (
+        <>
+            <h2>Intrebari frecvente</h2>
+            <>
+                <details>
+                    <summary>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+                    </summary>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua
+                    </p>
+                </details>
+                <details>
+                    <summary>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+                    </summary>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua
+                    </p>
+                </details>
+                <details>
+                    <summary>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+                    </summary>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua
+                    </p>
+                </details>
+            </>
+        </>
+    );
+}
+
+export default FrequentQuestions;
