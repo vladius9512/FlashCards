73f871a437f54c06f229f88d969cd30c5aa4722e
vladius9512 <dabija_vlad_adrian@yahoo.com>
2023-11-17 14:24:57 +0200

    Added 2 svgs as images and saved them in assets folder. Added some style to the main div. Also added a transition for the swapping svg on the card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import FlashCard from "./components/FlashCard";
 import FrequentQuestions from "./components/FrequentQuestions";
+import style from "./styles/buttons.module.css";
+import image from "./assets/doctor.svg";
 
 function App() {
     const [cardInfo, setCardInfo] = useState([
@@ -15,6 +17,18 @@ function App() {
     ]);
     return (
         <>
+            <div className="title-image-container">
+                <div className="left-side-container">
+                    <h1>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod
+                    </h1>
+                    <button className={style.primaryBtn}>Incepe acum</button>
+                </div>
+                <div className="right-side-wrapper">
+                    <img src={image}></img>
+                </div>
+            </div>
             <FlashCard cardInformations={cardInfo[0]}></FlashCard>
             <FrequentQuestions></FrequentQuestions>
         </>
--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import swapSvg from "../assets/swap.svg";
 
 function FlashCard({ cardInformations }) {
     const [cardAnswer, setCardAnswer] = useState(false);
@@ -10,49 +11,12 @@ function FlashCard({ cardInformations }) {
             <div className="card-body">
                 <div className="card-front">
                     <p>{cardInformations.frontInfo}</p>
-                    <svg
-                        viewBox="0 0 21 21"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="#000000"
-                        width={50}
+                    <img
+                        src={swapSvg}
+                        alt="swap"
+                        width={30}
                         onClick={cardClick}
-                    >
-                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                        <g
-                            id="SVGRepo_tracerCarrier"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        ></g>
-                        <g id="SVGRepo_iconCarrier">
-                            {" "}
-                            <g
-                                fill="none"
-                                fillRule="evenodd"
-                                stroke="#000000"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                transform="matrix(0 1 -1 0 18.5 2.5)"
-                            >
-                                {" "}
-                                <g transform="matrix(0 -1 1 0 .5 16.5)">
-                                    {" "}
-                                    <path
-                                        d="m16 0v5h-5"
-                                        transform="matrix(0 1 1 0 11 -11)"
-                                    ></path>{" "}
-                                    <path d="m16 5c-2.8366699-3.33333333-5.6700033-5-8.5-5-2.82999674 0-5.32999674 1-7.5 3"></path>{" "}
-                                </g>{" "}
-                                <g transform="matrix(0 1 -1 0 14 1)">
-                                    {" "}
-                                    <path
-                                        d="m16 0v5h-5"
-                                        transform="matrix(0 1 1 0 11 -11)"
-                                    ></path>{" "}
-                                    <path d="m16 5c-2.8366699-3.33333333-5.6700033-5-8.5-5-2.82999674 0-5.32999674 1-7.5 3"></path>{" "}
-                                </g>{" "}
-                            </g>{" "}
-                        </g>
-                    </svg>
+                    />
                 </div>
                 <div className="card-back">
                     <div className="svg-wrapper">
@@ -94,49 +58,12 @@ function FlashCard({ cardInformations }) {
                         </svg>
                     </div>
                     <p>{cardInformations.backInfo}</p>
-                    <svg
-                        viewBox="0 0 21 21"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="#000000"
-                        width={100}
+                    <img
+                        src={swapSvg}
+                        alt="swap"
+                        width={30}
                         onClick={cardClick}
-                    >
-                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                        <g
-                            id="SVGRepo_tracerCarrier"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        ></g>
-                        <g id="SVGRepo_iconCarrier">
-                            {" "}
-                            <g
-                                fill="none"
-                                fillRule="evenodd"
-                                stroke="#000000"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                transform="matrix(0 1 -1 0 18.5 2.5)"
-                            >
-                                {" "}
-                                <g transform="matrix(0 -1 1 0 .5 16.5)">
-                                    {" "}
-                                    <path
-                                        d="m16 0v5h-5"
-                                        transform="matrix(0 1 1 0 11 -11)"
-                                    ></path>{" "}
-                                    <path d="m16 5c-2.8366699-3.33333333-5.6700033-5-8.5-5-2.82999674 0-5.32999674 1-7.5 3"></path>{" "}
-                                </g>{" "}
-                                <g transform="matrix(0 1 -1 0 14 1)">
-                                    {" "}
-                                    <path
-                                        d="m16 0v5h-5"
-                                        transform="matrix(0 1 1 0 11 -11)"
-                                    ></path>{" "}
-                                    <path d="m16 5c-2.8366699-3.33333333-5.6700033-5-8.5-5-2.82999674 0-5.32999674 1-7.5 3"></path>{" "}
-                                </g>{" "}
-                            </g>{" "}
-                        </g>
-                    </svg>
+                    />
                 </div>
             </div>
         </div>
